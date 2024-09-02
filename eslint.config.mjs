dddd54e146037b6e8f0f0c7002e76d5671a0ab3a
Kevin Chavez <mr.kev@me.com>
2024-09-02 02:33:16 -0600

    update eslint to flat config, fix linting and update eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,85 @@
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import react from "eslint-plugin-react";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import reactHooks from "eslint-plugin-react-hooks";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import tseslint from "typescript-eslint";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default tseslint.config(
+  ...tseslint.configs.recommended,
+  {
+    ignores: ["**/vite.config.mts", "faustLoader/**/*", "src/midi/**/*", "public", "scripts", "packages", "build"],
+  },
+  ...fixupConfigRules(compat.extends("plugin:react/recommended", "plugin:react-hooks/recommended")),
+  {
+    plugins: {
+      react: fixupPluginRules(react),
+      "@typescript-eslint": typescriptEslint,
+      "react-hooks": fixupPluginRules(reactHooks),
+    },
+
+    languageOptions: {
+      globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+      },
+
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+
+    rules: {
+      "no-useless-concat": "off",
+      "@typescript-eslint/no-floating-promises": "error",
+
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "object-shorthand": "off",
+      "@typescript-eslint/no-confusing-void-expression": "off",
+      "@typescript-eslint/no-misused-promises": "off",
+      "@typescript-eslint/return-await": "off",
+      "no-useless-escape": "off",
+      "@typescript-eslint/consistent-type-imports": "off",
+      "@typescript-eslint/array-type": "off",
+      "@typescript-eslint/lines-between-class-members": "off",
+      "@typescript-eslint/consistent-generic-constructors": "off",
+      "spaced-comment": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react/react-in-jsx-scope": "off",
+      "react/no-unescaped-entities": "off",
+    },
+  }
+);
