651c3c1871c244d3b1512599bd7df2af5b60bee4
Kevin Chavez <mr.kev@me.com>
2024-12-26 22:11:47 -0600

    update dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,16 @@ import { App } from "./ui/App";
 import { nullthrows } from "./utils/nullthrows";
 import { appEnvironment } from "./lib/AppEnvironment";
 import { liveAudioContext } from "./constants";
+import { BrowserRouter } from "react-router";
+
 // initialize early
 void appEnvironment.initAsync(liveAudioContext());
 
 const root = createRoot(nullthrows(document.getElementById("root")));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
